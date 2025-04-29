b73b058d4539ca124915bba5aebd4160a4e94479
Yash Kumar Gupta <ykgupta2411@gmail.com>
2025-04-29 14:38:08 +0530

    Added /emergency/report page with new layout and background

--- resources/views/emergency/report.blade.php
+++ resources/views/emergency/report.blade.php
@@ -1,10 +1,64 @@
-<form action="/emergency/report" method="POST">
-    @csrf
-    <label>Type of Emergency</label>
-    <input type="text" name="type" required>
+<style>
+    .content{
+        width: 30%;
+        height: 80%;
+        margin-left: 80px;
+        padding: 20px;
+        background-color: white;
+        border-radius: 10px;
+        box-shadow: 3px 3px 5px 1px rgb(255, 210, 210);
+    }
 
-    <label>Your Location</label>
-    <input type="text" name="location" required>
+    .content .logo{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
-    <button type="submit">Report Emergency</button>
-</form>
+    .content form{
+        display: grid;
+        grid-template-rows: 3fr 1fr;
+        height: calc(100% - 80px);
+        margin-bottom: 0px;
+    }
+
+    .content .top{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .content input{
+        color: #1F2937;
+    }
+</style>
+
+<x-emergency-layout>
+    <div class="content">
+        <div class="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </div>
+        <form action="/emergency/report" method="POST">
+            @csrf
+            <div class="top">
+                <div>
+                    <x-input-label for="type" :value="__('Type of Emergency')" />
+                    <x-text-input id="type" class="block mt-1 w-full" type="text" name="type" :value="old('type')" required autofocus autocomplete="type" />
+                    <x-input-error :messages="$errors->get('type')" class="mt-2" />
+                </div>
+                <div class="mt-4">
+                    <x-input-label for="location" :value="__('Your Location')" />
+                    <x-text-input id="location" class="block mt-1 w-full" type="text" name="location" :value="old('location')" required autofocus autocomplete="location" />
+                    <x-input-error :messages="$errors->get('location')" class="mt-2" />
+                </div>
+            </div>
+            <div class="flex items-center justify-center mt-4">
+                <x-primary-button class="ms-3">
+                    {{ __('Report Emergency') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </div>
+</x-emergency-layout>
---
+++ resources/views/layouts/emergency.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <style>
+        .guest-container{
+            width: 100%;
+            height: calc(100vh - 60px);
+            background-image: url('/bg2.png');
+            background-color: #FEB99E;
+            background-size: contain;
+            background-position: right;
+            background-repeat: no-repeat;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: center;
+            color: white;
+        }
+
+        .guest-container > footer{
+            width: 100%;
+            text-align: center;
+            font-size: 14px;
+            color:rgba(255, 255, 255, 0.7);
+            position: absolute;
+            bottom: 0;
+            padding: 10px 0px;
+        }
+    </style>
+    <body class="font-sans text-gray-900 antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.guest-navigation')
+
+            <div class="guest-container">
+                {{ $slot }}
+
+                <!-- Footer -->
+                <footer>
+                    &copy; {{ date('Y') }} MediConnect. All rights reserved.
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
